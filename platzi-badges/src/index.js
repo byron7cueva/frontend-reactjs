78c672664e918508ca8b733a4fa3d20b319e0206
byron7cueva <byron7cueva@gmail.com>
2020-01-11 08:47:06 -0500

    Hello con react

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -1,5 +1,7 @@
-const element = document.createElement("h1");
-element.innerText = "Hello, Platzi Bages!";
+import React from "react";
+import ReactDOM from "react-dom";
 
+const element = <h1>Hello, Platzi Badges!</h1>;
 const container = document.getElementById("app");
-container.appendChild(element);
+//ReactDOM(que_voy_renderizar, donde_voy_hacer_append)
+ReactDOM.render(element, container);
