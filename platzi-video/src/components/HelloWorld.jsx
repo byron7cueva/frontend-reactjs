02e4de6fccafdb124a8157a5dd4597ec50f0de15
byron7cueva <byron7cueva@gmail.com>
2020-01-23 10:58:59 -0500

    Agergando estructura del Header y los estilos

--- platzi-video/src/components/HelloWorld.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import '../assets/styles/App.scss';
-
-const HelloWorld = () => <h1>Hello world</h1>;
-
-export default HelloWorld;
---
+++ platzi-video/src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../assets/styles/App.scss';
+
+import Header from '../components/Header';
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
+export default App;
