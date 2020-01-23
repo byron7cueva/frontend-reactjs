7972237bfbd6a0e231e8edcd04e1c1a49f0b4a13
byron7cueva <byron7cueva@gmail.com>
2020-01-23 11:18:09 -0500

    Agregando la sección principal

--- platzi-video/src/containers/App.jsx
+++ platzi-video/src/containers/App.jsx
@@ -4,11 +4,22 @@ import '../assets/styles/App.scss';
 
 import Header from '../components/Header';
 import Search from '../components/Search';
+import Category from '../components/Category';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CorouselItem';
 
 const App = () => (
   <div className="App">
     <Header />
     <Search />
+    <Category>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Category>
   </div>
 );
 
