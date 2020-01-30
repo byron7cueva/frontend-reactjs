ec5271d508d50a3eee10991797ad4f9984b1b1c6
byron7cueva <byron7cueva@gmail.com>
2020-01-30 16:40:07 -0500

    Mejorando el performance

--- petgram/src/pages/Detail.js
+++ petgram/src/pages/Detail.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
 import { PhotoCardWithQuery } from '../container/PhotoCardWithQuery'
+import { Layout } from '../components/Layout'
 
-export const Detail = ({ detailId }) => <PhotoCardWithQuery id={detailId} />
+export const Detail = ({ detailId }) => (
+  <Layout title={`Fotografía ${detailId}`}>
+    <PhotoCardWithQuery id={detailId} />
+  </Layout>
+)
--- petgram/src/pages/Home.js
+++ petgram/src/pages/Home.js
@@ -1,11 +1,20 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCard } from '../container/ListOfPhotoCard'
+import { Layout } from '../components/Layout'
 
-export const Home = ({ categoryId }) => (
-  <Fragment>
+const HomePage = ({ categoryId }) => (
+  <Layout
+    title="Tu app de fotos de mascotas"
+    subtitle="Con Petgram puedes encontrar fotos de animales domésticos muy bonitos"
+  >
     <ListOfCategories />
     <ListOfPhotoCard categoryId={categoryId} />
-  </Fragment>
+  </Layout>
 )
+
+//Indicando que no se renderice mientras no cambie categoryId
+export const Home = React.memo(HomePage, (prevProps, props) => {
+  return prevProps.categoryId === props.categoryId
+})
