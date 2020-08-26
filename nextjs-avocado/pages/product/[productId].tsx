89be7037cbf349f0dc3c4f1096152df7648fad79
byron7cueva <byron7cueva@gmail.com>
2020-08-26 10:59:08 -0500

    Connect to API product/id

--- nextjs-avocado/pages/product/[productId].tsx
+++ nextjs-avocado/pages/product/[productId].tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 // useRouter
 // Hook que nos permite hacer uso del touter que tiene next
 
 const Product = () => {
+  const [product, setProduct] = useState<TProduct | null>(null)
   const {
     query: { productId },
   } = useRouter()
+  
+  useEffect(() => {
+    if (productId) {
+      window.fetch(`/api/avo/${productId}`)
+      .then(response => response.json())
+      .then(data => setProduct(data))
+      .catch(console.error)
+    }
+  }, [productId])
 
-  return <div>Esta es la pagina de prueba de producto {productId}</div>
+  if (!product) return <p>El producto no existe</p>
+
+  return (
+    <div>
+      <h2>{product?.name}</h2>
+      <p><strong>Price: </strong>{product?.price}</p>
+      <h3>Descripción</h3>
+      <p>{product?.attributes.description}</p>
+    </div>
+  )
 }
 
 export default Product
