452851360452c18356231f04ae7c35ecadcdfff8
byron7cueva <byron7cueva@gmail.com>
2020-08-27 15:38:47 -0500

    redux - redux-basic - Config css loader

--- redux/redux-basic/src/index.js
+++ redux/redux-basic/src/index.js
@@ -1,1 +1,1 @@
-console.log('Hola Mundo')
+import './assets/css/styles.css'
--- redux/redux-basic/webpack.config.js
+++ redux/redux-basic/webpack.config.js
@@ -1,6 +1,7 @@
 
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -8,10 +9,26 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
   plugins: [
     new HtmlWebPackPlugin({
       template: './public/index.html',
       filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css'
     })
   ]
 }
