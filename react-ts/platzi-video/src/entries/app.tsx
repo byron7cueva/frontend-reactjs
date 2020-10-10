7fd94f7cfde12ccc8bd049f8a3d903fc937cb4d2
byron7cueva <byron7cueva@gmail.com>
2020-10-10 18:56:49 -0500

    react-ts - platzi-video - Refactor

--- react-ts/platzi-video/src/entries/app.tsx
+++ react-ts/platzi-video/src/entries/app.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import { Videos } from '../pages/container/Videos';
-import { Header } from '../pages/components/Header';
-import { Home } from '../pages/components/Home';
-import { NotFound } from '../pages/components/NotFound';
-import { store } from '../store/store';
+import { App } from '../pages/container/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/videos" component={Videos} />
-            <Redirect from="/v" to="/videos" />
-            <Route component={NotFound} />
-          </Switch>
-        </>
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
