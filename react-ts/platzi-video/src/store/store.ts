a481b6ddd4a2fcd0748599a0823fc91d7be68fb1
byron7cueva <byron7cueva@gmail.com>
2020-10-10 01:14:37 -0500

    react-ts - platzi-video - Multiple middleware

--- react-ts/platzi-video/src/store/store.ts
+++ react-ts/platzi-video/src/store/store.ts
@@ -1,7 +1,11 @@
 import { createStore, Store, applyMiddleware } from "redux";
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { rootReducer } from './reducers';
 
+// Custom middleware
+/*
 const logger = ({getState, dispatch}) => next => action => {
   console.log('Viejo estado', getState());
   console.log('Enviar la accion', action);
@@ -9,10 +13,11 @@ const logger = ({getState, dispatch}) => next => action => {
   console.log('Nuevo estado', getState());
   return value;
 }
+*/
 
 export const store: Store = createStore(
   rootReducer,
   {},
-  applyMiddleware(logger)
+  composeWithDevTools(applyMiddleware(logger))
   //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
