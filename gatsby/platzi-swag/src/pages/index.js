ae0d9488fbd286f365669843507198edd47e0b08
byron7cueva <byron7cueva@gmail.com>
2020-10-20 07:58:13 -0500

    gatsby - platzi-swag - Add products

--- gatsby/platzi-swag/src/pages/index.js
+++ gatsby/platzi-swag/src/pages/index.js
@@ -1,16 +1,15 @@
 import React from "react"
-import styled from 'styled-components'
 // Para conectarse al servidor de graphql de gatsby
 import { graphql } from "gatsby"
 
-import { SEO, Jumbo } from "../components"
+import { SEO, Jumbo, Products } from "../components"
 
 // Query para obtener la informacion del sitio
 // Se debe exportar el query
 // Gatsby va tomar el query, trae el resultado y lo va poner en el compenete como una prop "data"
 // Tag template literal
 export const query = graphql`
-  query GET_DESCRIPTION {
+  query GET_DATA {
     allSite {
       edges {
         node {
@@ -20,12 +19,30 @@ export const query = graphql`
         }
       }
     }
+    allStripePrice {
+      edges {
+        node {
+          id
+          unit_amount
+          unit_amount_decimal
+          type
+          product {
+            name
+            metadata {
+              description
+              img
+              wear
+            }
+          }
+        }
+      }
+    }
   }
 `
 
 // Genera una copia de un componente primitivo de HTML
 // Los estilos solo se mantienen para este componente y llegan al html con un id en especifico
-const Button = styled.button`
+/*const Button = styled.button`
   width: 8rem;
   background-color: #98ca3f;
   border: none;
@@ -35,14 +52,17 @@ const Button = styled.button`
   &:hover {
     transform: scale(1.4);
   }
-`
+`*/
 
-const IndexPage = ({ data }) => (
-  <>
-    <SEO title="Home" />
-    <Jumbo description={data.allSite.edges[0].node.siteMetadata.description}/>
-    <Button color="gray">Comprar</Button>
-  </>
-)
+const IndexPage = ({ data }) => {
+  console.log(data);
+  return (
+    <>
+      <SEO title="Home" />
+      <Jumbo description={data.allSite.edges[0].node.siteMetadata.description}/>
+      <Products products={data.allStripePrice.edges} />
+    </>
+  )
+}
 
 export default IndexPage
